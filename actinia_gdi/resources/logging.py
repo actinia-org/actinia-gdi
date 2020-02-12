a699ba3b1101a3f32162b5cc8572b6f139764f89
Carmen <carmen.tawalika@gmail.com>
2020-02-12 15:58:43 +0100

    enable json logging

--- actinia_gdi/resources/logging.py
+++ actinia_gdi/resources/logging.py
@@ -1,7 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Copyright (c) 2018-present mundialis GmbH & Co. KG
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+
+The logger
+"""
+
+__author__ = "Carmen Tawalika"
+__copyright__ = "2018-present mundialis GmbH & Co. KG"
+__license__ = "Apache-2.0"
+
+
 import logging
+from datetime import datetime
 from logging import FileHandler
 
 from colorlog import ColoredFormatter
+from pythonjsonlogger import jsonlogger
 from werkzeug.serving import WSGIRequestHandler
 
 from actinia_gdi.resources.config import LOGCONFIG
@@ -15,31 +43,77 @@ log = logging.getLogger('actinia-gdi')
 werkzeugLog = logging.getLogger('werkzeug')
 
 
+def setLogFormat(veto=None):
+    logformat = ""
+    if LOGCONFIG.type == 'json' and not veto:
+        logformat = CustomJsonFormatter('(time) (level) (component) (module)'
+                                        '(message) (pathname) (lineno)'
+                                        '(processName) (threadName)')
+    else:
+        logformat = ColoredFormatter(
+            '%(log_color)s[%(asctime)s] %(levelname)-10s: %(name)s.%(module)-'
+            '10s -%(message)s [in %(pathname)s:%(lineno)d]%(reset)s'
+        )
+    return logformat
+
+
+def setWerkzeugFormat():
+    werkzeugLogformat = ""
+    if LOGCONFIG.type == 'json':
+        werkzeugLogformat = CustomJsonFormatter('(time) (level) (component) '
+                                                '(message) (processName) '
+                                                '(threadName)')
+    else:
+        werkzeugLogformat = ColoredFormatter(
+            '%(log_color)s[%(asctime)s] %(levelname)-10s: %(name)-22s -'
+            '%(message)s %(reset)s'
+        )
+    return werkzeugLogformat
+
+
+class CustomJsonFormatter(jsonlogger.JsonFormatter):
+    def add_fields(self, log_record, record, message_dict):
+        super(CustomJsonFormatter, self).add_fields(log_record, record, message_dict)
+
+        # (Pdb) dir(record)
+        # ... 'args', 'created', 'exc_info', 'exc_text', 'filename', 'funcName'
+        # ,'getMessage', 'levelname', 'levelno', 'lineno', 'message', 'module',
+        # 'msecs', 'msg', 'name', 'pathname', 'process', 'processName',
+        # 'relativeCreated', 'stack_info', 'thread', 'threadName']
+
+        if not log_record.get('timestamp'):
+            # this doesn't use record.created, so it is slightly off
+            now = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+            log_record['time'] = now
+        if log_record.get('level'):
+            log_record['level'] = log_record['level'].upper()
+        else:
+            log_record['level'] = record.levelname
+
+        log_record['component'] = record.name
+
+
 def createLogger():
     # create logger, set level and define format
     log.setLevel(getattr(logging, LOGCONFIG.level))
-    logformat = ColoredFormatter(
-      '%(log_color)s[%(asctime)s] %(levelname)-10s: %(name)s.%(module)-10s -'
-      '%(message)s [in %(pathname)s:%(lineno)d]%(reset)s'
-    )
+    fileformat = setLogFormat('veto')
+    stdoutformat = setLogFormat()
 
     # Setup the logger handler for stdout
     handler = logging.StreamHandler()
-    handler.setFormatter(logformat)
+    handler.setFormatter(stdoutformat)
     log.addHandler(handler)
 
-    # Setup the logger handler for filesystem
+    # Setup the logger handler for filesystem. If json, it still is not json
+    # for readability
     file_handler = FileHandler(LOGCONFIG.logfile)
-    file_handler.setFormatter(logformat)
+    file_handler.setFormatter(fileformat)
     log.addHandler(file_handler)
 
 
 def createWerkzeugLogger():
     werkzeugLog.setLevel(getattr(logging, LOGCONFIG.level))
-    werkzeugLogformat = ColoredFormatter(
-      '%(log_color)s[%(asctime)s] %(levelname)-10s: %(name)-22s -'
-      '%(message)s %(reset)s'
-    )
+    werkzeugLogformat = setWerkzeugFormat()
 
     # Setup the logger handler for stdout
     werkzeugHandler = logging.StreamHandler()
@@ -59,9 +133,6 @@ class MyRequestHandler(WSGIRequestHandler):
             self.address_string() + ": " + (message % args)
         )
 
-
-# TODO: delete line if not needed
-# logging.root.setLevel(getattr(logging, LOGCONFIG.level))
 
 createLogger()
 createWerkzeugLogger()
