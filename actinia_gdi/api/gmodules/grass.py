6ad267cee24d9e3db0473520296eaecc6af1d99d
Carmen <carmen.tawalika@gmail.com>
2021-01-21 14:13:52 +0100

    unify python file header

--- actinia_gdi/api/gmodules/grass.py
+++ actinia_gdi/api/gmodules/grass.py
@@ -1,34 +1,33 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#######
-# actinia-core - an open source REST API for scalable, distributed, high
-# performance processing of geographical data that uses GRASS GIS for
-# computational tasks. For details, see https://actinia.mundialis.de/
-#
-# Copyright (c) 2016-2018 Sören Gebbert and mundialis GmbH & Co. KG
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-#
-#######
-
 """
-Module management
+Copyright (c) 2018-present mundialis GmbH & Co. KG
 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+
+Module management
 
 * List all modules
 * Describe single module
-
 """
+
+__license__ = "Apache-2.0"
+__author__ = "Anika Bettge, Carmen Tawalika"
+__copyright__ = "Copyright 2019, mundialis"
+__maintainer__ = "Anika Bettge, Carmen Tawalika"
+
+
 from flask import jsonify, make_response
 from flask_restful_swagger_2 import swagger
 from actinia_core.resources.resource_base import ResourceBase
@@ -37,12 +36,6 @@ from actinia_gdi.apidocs import gmodules
 from actinia_gdi.core.gmodulesGrass import createModuleList, createGrassModule
 from actinia_gdi.model.gmodules import ModuleList
 from actinia_gdi.model.responseModels import SimpleStatusCodeResponseModel
-
-
-__license__ = "GPLv3"
-__author__ = "Anika Bettge, Carmen Tawalika"
-__copyright__ = "Copyright 2019, mundialis"
-__maintainer__ = "Anika Bettge, Carmen Tawalika"
 
 
 class ListModules(ResourceBase):
