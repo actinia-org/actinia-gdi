96850f98f9f24214ba295036afbdac9cd05fe870
Carmen <carmen.tawalika@gmail.com>
2021-02-12 13:59:57 +0100

    split tests in plugin mode and standalone app

--- tests/tests_common/test_resources_cli.py
+++ tests/tests_common/test_resources_cli.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Copyright (c) 2018-present mundialis GmbH & Co. KG
+Copyright (c) 2018-2021 mundialis GmbH & Co. KG
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -19,9 +19,9 @@ limitations under the License.
 Test
 """
 
-__author__ = "Carmen Tawalika"
-__copyright__ = "2018-present mundialis GmbH & Co. KG"
 __license__ = "Apache-2.0"
+__author__ = "Carmen Tawalika"
+__copyright__ = "Copyright 2018-2021, mundialis"
 
 
 import unittest
@@ -32,10 +32,10 @@ from actinia_gdi.resources.cli import name, about
 class CliTest(unittest.TestCase):
 
     def test_cli_name(self):
-        assert name() == "Actinia GDI"
+        assert name() == "actinia-gdi"
 
     def test_cli_about(self):
-        t = "Actinia GDI"
+        t = "actinia-gdi"
         t = t + "\n This package communicates via HTTP"
         t = t + "\n To start application, run 'python -m actinia_gdi.main'"
 
